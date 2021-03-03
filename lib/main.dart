e99bbcf0d7b711df94845810a06adade50af2065
fviel <fernandovie82@gmail.com>
2021-03-02 23:57:01 -0300

    alterei o pubspec pra listar o path da imagem por completo, pois o flutter não estava encontrando a imagem

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,10 @@ import 'investimentos/util/util.dart';
 // Ordem ordem =  new Ordem(new Acao("ITSA4", 11.42, "ITAU SA"), 10, TipoOrdem.compra);
 
 //MaterialApp é o wrapper
-void main() => runApp(MaterialApp(
-    //Estou retornando uma classe, que na verdade é um widget, com isso tenho o hotreload
-    home: Home()));
+void main() =>
+    runApp(MaterialApp(
+      //Estou retornando uma classe, que na verdade é um widget, com isso tenho o hotreload
+        home: Home()));
 
 /**
  * Minha classe principal, com meu código, que retorna um Widget pra home lá no método main();
@@ -20,43 +21,49 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     //return Container();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Olá Fernando'),
-        centerTitle: true,
-        backgroundColor: Colors.purple,
-      ),
-      body: Container(
-          padding: EdgeInsets.all(10.0),
-          margin: EdgeInsets.all(30.0),
-          color: Colors.grey[100],
-          child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Row(
-                    children:<Widget>[
-                      Text('Fernando '),
-                      Text('Viel'),
-                    ]
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(20.0),
+        appBar: AppBar(
+          title: Text('Olá Fernando'),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+        ),
+        body: Row(
+          // mainAxisAlignment: MainAxisAlignment.end,
+          // crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+               child: Image.asset('assets/images/imagem3.jpg'),
+                ),
+              Expanded(
+                flex:3,
+                child: Container(
+                  padding: EdgeInsets.all(5.0),
+                  color: Colors.amber,
+                  child: Text('dois'),
+                ),
+              ),
+              Expanded(
+                flex:2,
+                  child: Container(
+                    padding: EdgeInsets.all(5.0),
                     color: Colors.cyan,
                     child: Text('um'),
                   ),
-                  Container(
-                    padding: EdgeInsets.all(30.0),
-                    color: Colors.amber,
-                    child: Text('dois'),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(40.0),
-                    color: Colors.yellow,
-                    child: Text('três'),
-                  ),
-                ]),
-          )),
+              ),
+              Expanded(
+                flex:1,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(5.0),
+                      color: Colors.yellow,
+                      child: Text('três'),
+                    ),
+                  ],
+                ),
+              ),
+            ]
+        ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('click'),
